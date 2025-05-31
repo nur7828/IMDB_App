a64785c5aea68bd3d29242e19cdd6f604f74d97f
Nur Islam <nurislam7828@gmail.com>
2025-05-31 16:34:19 +0530

    Remove API key file from Git tracking

--- src/API_KEY/constant.js
+++
@@ -1,6 +0,0 @@
-export const API_KEY = "6fa33f0f92ed0466e1c3dd84be33a4bd";
-
-//base path for img
-export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
-
-export const WATCHLIST_KEY = "WATCHLIST_KEY";
